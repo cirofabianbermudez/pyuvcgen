34664dc8183e8a0f7a0a7b37f073e76f6178f758
Ciro Bermudez <cirofabian.bermudez@gmail.com>
2025-06-13 12:27:57 +0200

    feat: update init.py to get the version from pyproject.toml

--- src/pyuvcgen/__init__.py
+++ src/pyuvcgen/__init__.py
@@ -1,4 +1,9 @@
-__version__ = "0.1.0"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pyuvcgen")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 from .generator import generate_uvc
 
